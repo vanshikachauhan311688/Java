194858d984f8e52d3234c5ce579d6dbf6ca79e83
vanshikachauhan311688 <vcvanshika31@gmail.com>
2026-03-15 14:55:58 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void main(String[] args) {
+        double r=349;
+        double pi=3.1415;
+        double area=pi*r*r;
+        System.out.println(area);
+    }
+}
